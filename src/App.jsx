d62cde1a11fe473d99317281880a1e1ebdb41cfd
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-08-18 19:44:17 -0300

    deletar tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import styled from "styled-components";
 import { FaTrashAlt } from "react-icons/fa";
 
-
-
 const Main = styled.main`
   display: flex;
   flex-direction: column;
@@ -17,7 +15,7 @@ const ContainerTarefas = styled.div`
   flex-direction: column;
   text-align: center;
   margin-top: 1.5rem;
-`
+`;
 
 const FormConteudo = styled.form`
   display: flex;
@@ -31,31 +29,35 @@ const Input = styled.input`
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
   border: 0;
   outline: none;
-
 `;
 
 const Tarefa = styled.p`
   display: flex;
   align-items: center;
+  justify-content: center;
   gap: 16px;
   padding: 1rem;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  transition: .2s;
-  &:hover{
-      font-size: 1.1rem;
-      cursor: pointer;
+  transition: 0.2s;
+  &:hover {
+    font-size: 1.1rem;
+    cursor: pointer;
   }
-`
-
+`;
 
 function App() {
   const [input, setInput] = useState("");
-  const [tarefas, setTarefas] = useState([])
+  const [tarefas, setTarefas] = useState([]);
 
-  function handleSubmit(e){
-    e.preventDefault()
-    setTarefas([...tarefas, input])
-    setInput('')
+  function handleSubmit(e) {
+    e.preventDefault();
+    const novaTarefa = { input: input, id: Date.now().toString()};
+    setTarefas((prevTarefa) => [...prevTarefa, novaTarefa]);
+    setInput("");
+  }
+
+  function deleteTask(id) {
+    return setTarefas((prevTarefas) => prevTarefas.filter(tarefa => tarefa.id !== id));
   }
 
   return (
@@ -63,17 +65,22 @@ function App() {
       <Main>
         <FormConteudo onSubmit={handleSubmit}>
           <h1>Digite uma tarefa...</h1>
-          <Input 
-            type="text" 
-            placeholder="Digite sua tarefa..." 
-            value={input} 
-            onChange={(e => setInput(e.target.value))}
+          <Input
+            type="text"
+            placeholder="Digite sua tarefa..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
         </FormConteudo>
         <ContainerTarefas>
           {tarefas.map((tarefa, index) => {
-            return <Tarefa key={index}>{tarefa} </Tarefa>
-            // <FaTrashAlt color="red" size={19} />
+            return (
+              <Tarefa key={index}>
+                {tarefa.input}
+                <FaTrashAlt color="red" size={19} onClick={() => deleteTask(tarefa.id)} />
+              </Tarefa>
+            );
+
           })}
         </ContainerTarefas>
       </Main>
